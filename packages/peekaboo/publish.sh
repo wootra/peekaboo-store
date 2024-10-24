9708dd7e1c7c051eeaf2adc205cc7ea5a99d42c7
Songhyeon Jun <shjeon0730@gmail.com>
2024-10-24 00:46:48 -0500

    update publish script

--- packages/peekaboo/publish.sh
+++ packages/peekaboo/publish.sh
@@ -7,6 +7,7 @@ fi
 
 echo "version is $VERSION"
 pnpm run lint && echo 'lint success' || exit 1
+pnpm run clear && echo 'clear success'
 pnpm rebuild && echo 'build success'|| exit 1
 
 echo $VERSION
